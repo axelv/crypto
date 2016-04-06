b16615947a1057fc7e6444a977a74e027f3fb429
axelvanraes <axelvanraes@hotmail.com>
2016-04-06 07:29:06 -0400

    Restructuring STS files

--- bbs/bbs.c
+++ bbs/bbs.c
@@ -10,41 +10,36 @@
  */
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Choose two primes p & q so that each congruent to 3 mod 4, then n = p*q. */
 	
 #define n (1049*1061) // = 2109
-// The maximum possible length for the random bitstream.
-#define MAX_l 128
+// The maximum possible length (in bits) for the random bitstream.
+#define MAX_l 256
 /* ------------------------------------------------------------------------- */
 
 void random_byte(unsigned char *byte, unsigned *seed){
 	unsigned i;
 	for (i=0; i<8;i++){
-		*seed = (*seed)*(*seed) % n;
+		*seed = (*seed)*(*seed) % n; // the new seed: s(n) = s(n-1)² mod n
 		*byte +=((*seed & 1) << i); // Select the LSB of seed en put in the bit with index i
 	}
 }
 
 /* ------------------------------------------------------------------------- */
 
-int main() {
+int generate_number(uint8_t *result, uint8_t len_bits, uint8_t seed) {
 	
-	// The 'seed' has to be a value between 1 & n-1 such that gcd(s,n) = 1
-	unsigned s;
-	unsigned char x[MAX_l/8] = {0,};
-	unsigned l = MAX_l/8; //Number of random bytes needed
+	unsigned l = max(MAX_l/8, len_bits/8); //Number of random bytes needed
 	unsigned i;
-	printf("Give a seed: \n" );
-	scanf("%d", &s);
-	s = (s % n-1)+1;	
+	// The 'seed' has to be a value between 1 & n-1 such that gcd(s,n) = 1
+	// TODO: check when the seed = n-2
+	seed = (seed % n-1)+1;	
 	
 	for(i=0; i < l; i++){
-		random_byte(x+i,&s);
-		printf("%d: %02X \n",i,x[i]);
-	}
-	printf("\n Done \n");
-	
+		random_byte(result+i,&seed);
+	}	
 }
 
 
