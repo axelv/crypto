60c98f77ad034c8209a21d90f73253cb0d050174
Michael Raes <mraes@me.com>
2016-04-30 12:44:32 +0200

    Little comment change

--- asymmetric/monmult.h
+++ asymmetric/monmult.h
@@ -15,7 +15,7 @@
 #ifndef __MONMULT_H__
 #define __MONMULT_H__
 #define SIZE 156 // size of modulus in BYTES
-#define MWORDSIZE 8 // in bits
+#define MWORDSIZE 8 // Montgomery word size in bits
 #define MONT_DEBUG 0
 
 void montgomery_multiplication(uint8_t *res, uint8_t *in1, uint8_t *in2, uint8_t *n);
