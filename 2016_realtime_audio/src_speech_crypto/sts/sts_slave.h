7f343bdd40af0967c4a9ec495f1ec5d113e47de9
mraes <mraes@me.com>
2016-05-14 11:35:46 +0200

    Ayy

--- 2016_realtime_audio/src_speech_crypto/sts/sts_slave.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#ifndef STS_SLAVE_H
-#define STS_SLAVE_H
-//Shared key for AES
-extern uint8_t SKEY[PSIZE];
-
-void reply_master(uint8_t message[PSIZE+HASHSIZE], uint8_t received_msg[PSIZE], uint8_t *seqnr);
-uint8_t validate_master(uint8_t *message, uint8_t *seqnr);
-#endif
